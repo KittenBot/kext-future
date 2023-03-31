a5f99c5617752bb60179e76fccbe9a3f1aab186d
Si-ege <1660119581@qq.com>
2023-03-31 11:33:49 +0800

    删除图片转字节 hook

--- extra.js
+++ extra.js
@@ -5,18 +5,4 @@
     var B = args.B
     return "#" + (1 << 24 | R << 16 | G << 8 | B).toString(16).slice(1)
   },
-  argsHook: {
-    PATH: (args,gen)=>{
-      let unicode = '';
-      for (let i = 0; i < args.length; i++) {
-        let code = args.charCodeAt(i)
-        if (code >= 0x4e00 && code <= 0x9fa5) {
-          unicode += "\\u" + code.toString(16);
-        } else {
-          unicode += args[i];
-        }
-      }
-      return `'${unicode}'`
-    }
-  }
 }
